122256b43cb7cffa5403ef4be1b2626d9abca6f5
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 21:57:14 +0100

    ✨feature: Création de la page Contact, avec son formulaire de contact et son fichier d'envoi de mail

---
+++ contact.php
@@ -0,0 +1,103 @@
+<?php
+
+require("system/core.php");
+
+$Functions->summonPage("Contact");
+
+?>
+
+<div class="container mt-5">
+
+	<section class="mb-4">
+
+		<!--Section heading-->
+		<h2 class="h1-responsive font-weight-bold text-center my-4">Contact </h2>
+
+		<div class="row">
+
+			<!--Grid column-->
+			<div class="col-md-9 mb-md-0 mb-5">
+				<form id="contact-form" name="contact-form" action="system/mail.php" method="POST">
+
+					<!--Grid row-->
+					<div class="row pb-2">
+
+						<!--Grid column-->
+						<div class="col-md-6">
+							<div class="md-form mb-0">
+								<input type="text" id="name" name="name" class="form-control">
+								<label for="name" class="">Votre nom</label>
+							</div>
+						</div>
+						<!--Grid column-->
+
+						<!--Grid column-->
+						<div class="col-md-6">
+							<div class="md-form mb-0">
+								<input type="text" id="email" name="email" class="form-control">
+								<label for="email" class="">Votre email</label>
+							</div>
+						</div>
+						<!--Grid column-->
+
+					</div>
+					<!--Grid row-->
+
+					<!--Grid row-->
+					<div class="row pb-2">
+						<div class="col-md-12">
+							<div class="md-form mb-0">
+								<input type="text" id="subject" name="subject" class="form-control">
+								<label for="subject" class="">Sujet</label>
+							</div>
+						</div>
+					</div>
+					<!--Grid row-->
+
+					<!--Grid row-->
+					<div class="row pb-2">
+
+						<!--Grid column-->
+						<div class="col-md-12">
+
+							<div class="md-form">
+								<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+								<label for="message">Votre message</label>
+							</div>
+
+						</div>
+					</div>
+					<!--Grid row-->
+
+				</form>
+
+				<div class="text-center text-md-left">
+					<a class="btn btn-primary">Envoyer</a>
+				</div>
+				<div class="status"></div>
+			</div>
+			<!--Grid column-->
+
+			<!--Grid column-->
+			<div class="col-md-3 text-center">
+				<ul class="list-unstyled mb-0">
+					<li><i class="fas fa-phone mt-4 fa-2x"></i>
+						<p>07 78 56 07 25</p>
+					</li>
+
+					<li><i class="fas fa-envelope mt-4 fa-2x"></i>
+						<p>perso@stan1712.com</p>
+					</li>
+				</ul>
+			</div>
+			<!--Grid column-->
+
+		</div>
+
+		<hr class="mb-5 mt-4">
+	</section>
+
+</div>
+
+
+<?php include "includes/footer.php"; ?>
---
+++ system/mail.php
@@ -0,0 +1,19 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+
+$content="
+	Expediteur : $name
+	\n Email : $email
+	\n Message : $message
+";
+$mailheader = "From: $email \r\n";
+
+mail("perso@stan1712.com", $subject, $content, $mailheader) or die("Erreur !");
+echo "L'email à été envoyé !";
