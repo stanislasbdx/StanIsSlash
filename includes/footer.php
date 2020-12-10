be1ebd625ca090f2ff0513e0d68396a34e546a68
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 23:15:46 +0100

    ✨feature: Ajout de la mention "projet open source"

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer font-small blue">
-	<div class="footer-copyright text-center py-3">© <?= Date("Y") ?> - <a href="https://stan1712.com" target="_blank">Stani/</a> <small>(idée originale du pseudo par Joran Escudié, B2 EPSI)</small>
-	</div>
+	<div class="footer-copyright text-center">© <?= Date("Y") ?> - <a href="https://stan1712.com" target="_blank">Stani/</a> <small>(idée originale du pseudo par Joran Escudié, B2 EPSI)</small></div>
+	<div class="footer-copyright text-center font-weight-light pb-3"><a href="https://github.com/stan1712/StanIsSlash" target="_blank">Project en Open Source sur Github</a></div>
 </footer>
 
 <!-- MDB -->
