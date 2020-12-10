d000f2d42a67d30891f9f0644bcc16437c7c82b0
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 22:42:01 +0100

    🚅perfs: Ajout des champs "required" et de leurs types (refonte graphique)

--- contact.php
+++ contact.php
@@ -21,87 +21,64 @@ $Functions->summonPage("Contact");
 		<?php } ?>
 	<?php } ?>
 
-		<!--Section heading-->
+	<section class="mb-4">
 		<h2 class="h1-responsive font-weight-bold text-center my-4">Contact </h2>
 
-		<div class="row">
+		<div class="row pb-4">
 
-			<!--Grid column-->
 			<div class="col-md-9 mb-md-0 mb-5">
 				<form id="contact-form" name="contact-form" action="system/mail.php" method="POST">
 
-					<!--Grid row-->
 					<div class="row pb-2">
-
-						<!--Grid column-->
 						<div class="col-md-6">
 							<div class="md-form mb-0">
-								<input type="text" id="name" name="name" class="form-control">
+								<input type="input" id="name" name="name" class="form-control" required>
 								<label for="name" class="">Votre nom</label>
 							</div>
 						</div>
-						<!--Grid column-->
 
-						<!--Grid column-->
 						<div class="col-md-6">
 							<div class="md-form mb-0">
-								<input type="text" id="email" name="email" class="form-control">
+								<input type="email" id="email" name="email" class="form-control" required>
 								<label for="email" class="">Votre email</label>
 							</div>
 						</div>
-						<!--Grid column-->
-
 					</div>
-					<!--Grid row-->
 
-					<!--Grid row-->
 					<div class="row pb-2">
 						<div class="col-md-12">
 							<div class="md-form mb-0">
-								<input type="text" id="subject" name="subject" class="form-control">
+								<input type="input" id="subject" name="subject" class="form-control" required>
 								<label for="subject" class="">Sujet</label>
 							</div>
 						</div>
 					</div>
-					<!--Grid row-->
 
-					<!--Grid row-->
 					<div class="row pb-2">
-
-						<!--Grid column-->
 						<div class="col-md-12">
-
 							<div class="md-form">
-								<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+								<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" required></textarea>
 								<label for="message">Votre message</label>
 							</div>
-
 						</div>
 					</div>
-					<!--Grid row-->
+
+					<div class="text-center text-md-left">
+						<button class="btn btn-primary" type="submit">Envoyer</button>
+					</div>
 
 				</form>
-
-				<div class="text-center text-md-left">
-					<a class="btn btn-primary">Envoyer</a>
-				</div>
-				<div class="status"></div>
 			</div>
-			<!--Grid column-->
 
-			<!--Grid column-->
 			<div class="col-md-3 text-center">
 				<ul class="list-unstyled mb-0">
-					<li><i class="fas fa-phone mt-4 fa-2x"></i>
-						<p>07 78 56 07 25</p>
-					</li>
-
 					<li><i class="fas fa-envelope mt-4 fa-2x"></i>
 						<p>perso@stan1712.com</p>
 					</li>
 				</ul>
 			</div>
-			<!--Grid column-->
+
+		</div>
 
 		</div>
 
