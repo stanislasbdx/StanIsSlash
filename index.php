04e4fa338b347d86bf46be60e7009a75fd7715cf
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 21:56:34 +0100

    ✨feature: Création de la page Accueil, qui répertorie les Slashs

--- index.php
+++ index.php
@@ -6,7 +6,48 @@ $Functions->summonPage("Accueil");
 
 ?>
 
-<h1>Accueil</h1>
+<div class="container mt-5">
+
+	<section class="">
+
+
+		<h3 class="text-center font-weight-bold">Stan Is Slash, le blog avec les plus beaux slashs</h3>
+		<h4 class="text-center font-weight-light mb-5">Les plus belles punchs à utiliser en société.</h4>
+
+
+		<div class="row">
+
+			<?php $result = $BDD->query("SELECT * FROM frontender_blog");
+
+			foreach ($result as $row) { ?>
+				<div class="col-lg-4 col-md-12 mb-lg-0 mb-4 mt-4">
+					<div class="card hoverable">
+
+						<?php if ($row['image']) { ?>
+							<img class="card-img-top" src="<?= $row['image'] ?>"> <!-- IMAGE PREVIEW -->
+						<?php } ?>
+
+						<div class="card-body">
+
+							<h4><a href="article.php?q=<?= $row['id'] ?>"><?= $row['title'] ?></a></h4> <!-- TITRE DE LA CARD + LIEN DU POST -->
+
+							<p class="card-title text-muted font-small mt-3 mb-2"><?= substr($row['content'], 0, 150) . '...' ?></p> <!-- DESCRIPTION COURTE -->
+
+							<a type="button" class="btn btn-outline-primary mx-0" href="article.php?q=<?= $row['id'] ?>">Lire le slash<i class="fa fa-angle-right ml-2"></i></a> <!-- LIEN DU POST -->
+
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+
+			<!-- Ajouter ici les posts -->
+
+		</div>
+
+		<hr class="mb-5 mt-4">
+	</section>
+
+</div>
 
 
 <?php include "includes/footer.php"; ?>
