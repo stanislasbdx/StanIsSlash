a5159cd6e26b21fd07f5dd377561902b5d059b7d
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 21:56:09 +0100

    ✨feature: Habillage du site avec Material Design Bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,11 @@
+<footer class="page-footer font-small blue">
+	<div class="footer-copyright text-center py-3">© <?= Date("Y") ?> - <a href="https://stan1712.com" target="_blank">Stani/</a> <small>(idée originale du pseudo par Joran Escudié, B2 EPSI)</small>
+	</div>
+</footer>
+
+<!-- MDB -->
+<script type="text/javascript" src="assets/js/mdb.min.js"></script>
+
 </body>
 
 </html>
--- includes/head.php
+++ includes/head.php
@@ -2,11 +2,33 @@
 <html lang="fr">
 
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
+
 	<title><?= $title ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<link rel="stylesheet" href="assets/style.css" />
+
+	<link rel="shortcut icon" href="https://media.discordapp.net/attachments/610108081157963787/758399366356140042/Logo_new.jpg">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+	<!-- Google Fonts Roboto -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+	<!-- MDB -->
+	<link rel="stylesheet" href="assets/css/mdb.min.css" />
 </head>
 
 <body>
+
+<nav class="navbar navbar-expand-lg primary-color px-4">
+	<a class="navbar-brand bold" href="index.php">Stan Is Slash</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav px-4">
+			<li class="nav-item active">
+				<a class="nav-link" href="contact.php">Contact</a>
+			</li>
+		</ul>
+	</div>
+</nav>
