53de1a171498e51ce748bc7c49ee41e593f62c0f
Stanislas Castaybert <perso@stan1712.com>
2020-12-10 17:44:34 +0100

    🔵other: Précision sur la politique du gitignore et des credentials

--- system/core.php
+++ system/core.php
@@ -6,6 +6,6 @@ spl_autoload_register("loadClasses");
 
 $Functions = new Functions();
 
-require "private/db_logs.php";
+require "private/db_logs.php"; // Les credentials sont ignorés pour éviter une intrusion sur la base de donnée.
 $BDD = new mysqli($_servername, $_username, $_password, "hostcrus_projects");
 ?>
